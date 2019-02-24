4f27d0034b333486b321fd261c684026664cfbb3
CookeRichard94 <g00331787@gmit.ie>
2019-02-24 23:47:00 +0000

    Sixth Commit - Upcoming-Update Complete

--- src/app/upcoming-update/upcoming-update.component.ts
+++ src/app/upcoming-update/upcoming-update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Router, ActivatedRoute} from '@angular/router';
+import {UpcomingService} from '../services/upcoming.service';
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-upcoming-update',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpcomingUpdateComponent implements OnInit {
 
-  constructor() { }
+  upcoming : any = [];
+  constructor(private route:ActivatedRoute, private service:UpcomingService,
+  private router:Router) { }
 
   ngOnInit() {
+
+    console.log(this.route.snapshot.params['id']);
+
+    this.service.getPost(this.route.snapshot.params['id'])
+    .subscribe(data =>
+    {
+      this.upcoming = data;
+    });
+  }
+
+  onEditPost(form: NgForm){
+    this.service.updatePost(this.upcoming._id, form.value.country, form.value.city, form.value.year )
+    .subscribe(() => 
+  {
+    this.router.navigate(['/upcoming']);
+  });
   }
 
 }
