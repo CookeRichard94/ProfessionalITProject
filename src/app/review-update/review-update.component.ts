6d17af73396fc39f313e7d189fb977f1ce292f48
RICHARD COOKE - Student <G00331787@gmit.ie>
2019-02-25 12:35:53 +0000

    Tenth Commit - Review-Update Complete

--- src/app/review-update/review-update.component.ts
+++ src/app/review-update/review-update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Router, ActivatedRoute} from '@angular/router';
+import {ReviewService} from '../services/review.service';
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-review-update',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReviewUpdateComponent implements OnInit {
 
-  constructor() { }
+  post : any = [];
+  constructor(private route:ActivatedRoute, private service:ReviewService,
+  private router:Router) { }
 
   ngOnInit() {
+
+    console.log(this.route.snapshot.params['id']);
+
+    this.service.getPost(this.route.snapshot.params['id'])
+    .subscribe(data =>
+    {
+      this.post = data;
+    });
+  }
+
+  onEditPost(form: NgForm){
+    this.service.updatePost(this.post._id, form.value.title, form.value.content, form.value.city, form.value.rating)
+    .subscribe(() => 
+  {
+    this.router.navigate(['/review']);
+  });
   }
 
 }
