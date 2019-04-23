f59e38da6b6304d7777bdbcf209a5610de9f96ac
CookeRichard94 <33707699+CookeRichard94@users.noreply.github.com>
2019-04-23 16:37:49 +0100

    Added Review-Update Comments

--- src/app/review-update/review-update.component.ts
+++ src/app/review-update/review-update.component.ts
@@ -1,3 +1,4 @@
+//Imports
 import { Component, OnInit } from '@angular/core';
 import {Router, ActivatedRoute} from '@angular/router';
 import {ReviewService} from '../services/review.service';
@@ -11,6 +12,8 @@ import { NgForm } from "@angular/forms";
 export class ReviewUpdateComponent implements OnInit {
 
   post : any = [];
+  
+  //Draws the constructor from the ReviewService service
   constructor(private route:ActivatedRoute, private service:ReviewService,
   private router:Router) { }
 
@@ -18,6 +21,7 @@ export class ReviewUpdateComponent implements OnInit {
 
     console.log(this.route.snapshot.params['id']);
 
+    //Retrieves the data of the single specified item by it's id
     this.service.getPost(this.route.snapshot.params['id'])
     .subscribe(data =>
     {
@@ -25,10 +29,12 @@ export class ReviewUpdateComponent implements OnInit {
     });
   }
 
+  //updates the information of the item with the newly input information
   onEditPost(form: NgForm){
     this.service.updatePost(this.post._id, form.value.title, form.value.content, form.value.city, form.value.rating)
     .subscribe(() => 
   {
+      //sends the user back to the review list page
     this.router.navigate(['/review']);
   });
   }
