c0bb0b7da0b1b7f62eec69ee845b8b04e13ca3ae
CookeRichard94 <33707699+CookeRichard94@users.noreply.github.com>
2019-04-23 16:42:22 +0100

    Update upcoming-list.component.ts

--- src/app/upcoming-list/upcoming-list.component.ts
+++ src/app/upcoming-list/upcoming-list.component.ts
@@ -1,3 +1,4 @@
+//Imports
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import {Upcoming} from '../upcoming.model';
@@ -12,26 +13,28 @@ export class UpcomingListComponent implements OnInit {
 
   upcoming: any = [];
 
+  //uses the constructor for items specified in the UpcomingService service
   constructor(private ps:UpcomingService){}
 
   ngOnInit(){
 
+    //When the page is initiated all of the data in the relevant collection is drawn and presented
     this.ps.getPostsData().subscribe(data => {
         this.upcoming = data;
     });
-
-
    }
 
+  //Delete Method
    onDelete(id:String){
+     //Ouputs the id of the item to be deleetd to the console window
     console.log("Delete called " + id);
+     
+     //deletes the specified item by id from the collection
     this.ps.deletePost(id).subscribe(() =>
     {
+      //calls the init method again to refresh the page
       this.ngOnInit();
     });
 
    }
-
 }
-
-
