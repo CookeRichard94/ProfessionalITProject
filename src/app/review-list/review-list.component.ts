1cb73ad4a84bae186def59f49ef4c5e76700204c
CookeRichard94 <33707699+CookeRichard94@users.noreply.github.com>
2019-04-23 16:34:52 +0100

    Added Review-List comments

--- src/app/review-list/review-list.component.ts
+++ src/app/review-list/review-list.component.ts
@@ -1,3 +1,4 @@
+//imports
 import { Component, OnInit } from '@angular/core';
 import {ReviewService} from '../services/review.service';
 import { Observable } from 'rxjs';
@@ -12,24 +13,26 @@ export class ReviewListComponent implements OnInit {
 
   posts: any = [];
 
+  //Draws the constructors from the ReviewService service
   constructor(private ps:ReviewService){}
 
+  //When the page is opened the items are all drawn from the service
   ngOnInit(){
 
     this.ps.getPostsData().subscribe(data => {
         this.posts = data;
     });
-
-
    }
-
+  
+//Delete Method
    onDelete(id:String){
+     //Outputs the id of the item to be deleted to the console
     console.log("Delete called " + id);
+     //deletes the specified item and then re-initiates the page
     this.ps.deletePost(id).subscribe(() =>
     {
       this.ngOnInit();
     });
-
    }
 }
 
